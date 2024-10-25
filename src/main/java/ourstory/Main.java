43b8c4327f7e00833bd72e8725d41e4d0279f940
Guilhem <aze.adrarit@gmail.com>
2024-10-26 01:17:49 +0200

    Added base skin command

--- src/main/java/ourstory/Main.java
+++ src/main/java/ourstory/Main.java
@@ -1,6 +1,8 @@
 package ourstory;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -13,8 +15,11 @@ import ourstory.recipes.*;
 public class Main extends JavaPlugin {
 
 	public static final String namespace = "ourstory";
-	public static List<String> deathMessages;
-	public static List<String> tipMessages;
+	public static List<String> deathMessages, tipMessages;
+	public static List<CustomSkin> skins = new ArrayList<>();
+
+	public record CustomSkin(int id, String name, int price) {
+	}
 
 	@Override
 	public void onEnable() {
@@ -24,13 +29,15 @@ public class Main extends JavaPlugin {
 		deathMessages = getConfig().getStringList("deathMessages");
 		tipMessages = getConfig().getStringList("tipMessages");
 
+		loadSkins();
+
 		// Register task for running the periodic tip broadcast
 		new BukkitRunnable() {
 			@Override
 			public void run() {
 				onPlayerTips.playerTips();
 			}
-		}.runTaskTimer(this, 0L, 24000L);
+		}.runTaskTimer(this, 0L, 27000L);
 
 		/*
 		 * Registers all events
@@ -55,6 +62,7 @@ public class Main extends JavaPlugin {
 			final Commands commands = event.registrar();
 
 			commands.register("boss", "WIP", new Boss());
+			commands.register("skin", "Change the skin of your current weapon", new Skin());
 			commands.register("split", "Splits the enchants on your books", new Split());
 			commands.register("rankup", "Increases your rank", new RankUp());
 			commands.register("count", "Count items in your inventory", new Count());
@@ -70,6 +78,16 @@ public class Main extends JavaPlugin {
 		// this command");
 		// this.getConfig().options().copyDefaults(true);
 		// this.saveConfig();
+	}
+
+	private void loadSkins() {
+		for (Map<?, ?> map : getConfig().getMapList("skins")) {
+			int id = (int) map.get("id");
+			String name = (String) map.get("name");
+			int price = (int) map.get("price");
+
+			skins.add(new CustomSkin(id, name, price));
+		}
 	}
 
 	@Override
