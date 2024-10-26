ecd869d3858edbd60273f4eb4fc717bedb8b88fb
Guilhem <aze.adrarit@gmail.com>
2024-10-26 17:54:47 +0200

    Added fire aspect and flame edge cases to zombie case

--- src/main/java/ourstory/events/onZombieDeath.java
+++ src/main/java/ourstory/events/onZombieDeath.java
@@ -1,25 +1,53 @@
 package ourstory.events;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.bukkit.Material;
+import org.bukkit.damage.DamageSource;
+import org.bukkit.damage.DamageType;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import ourstory.utils.EnchantItem;
 
 public class onZombieDeath implements Listener {
 	/*
 	 * If a zombie if killed by fire, makes him drop leather instead of rotten flesh
 	 */
+	private final List<EntityType> zombies = Arrays.asList(
+			EntityType.ZOMBIE,
+			EntityType.HUSK,
+			EntityType.DROWNED,
+			EntityType.ZOMBIFIED_PIGLIN,
+			EntityType.ZOMBIE_VILLAGER,
+			EntityType.ZOGLIN);
+
+
 	@EventHandler
 	public void zombieDeath(EntityDeathEvent entity) {
-		if (entity.getEntityType() != EntityType.ZOMBIE)
+		if (!zombies.contains(entity.getEntityType()))
 			return;
 
-		if (entity.getEntity().getFireTicks() > 0) {
+		boolean killedByFire = false;
+		DamageSource source = entity.getDamageSource();
 
+		// Check if killed from fire_aspect
+		if (source.getDamageType() == DamageType.PLAYER_ATTACK) {
+			Player p = (Player) source.getCausingEntity();
+			ItemStack weapon = p.getInventory().getItemInMainHand();
+
+			killedByFire = EnchantItem.getEnchantAmount(weapon, "fire_aspect") > 0;
+		}
+
+		// Check if killed by flamed arrow
+		if (source.getDamageType() == DamageType.ARROW)
+			killedByFire = source.getDirectEntity().getFireTicks() > 0;
+
+		if (killedByFire || entity.getEntity().getFireTicks() > 0) {
 			List<ItemStack> temp = new ArrayList<>();
 
 			for (ItemStack drop : entity.getDrops())
