2fe85951fc9a9b7e71450d93d1c41e70f65b35a5
Guilhem <aze.adrarit@gmail.com>
2024-11-10 15:13:37 +0100

    Added reinforcedDeeplslate drop with silk touch

---
+++ src/main/java/ourstory/events/onMineDeepslate.java
@@ -0,0 +1,25 @@
+package ourstory.events;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class onMineDeepslate implements Listener {
+	/*
+	 * Allow players to mine Budding Amethyst if mined with silk touch
+	 */
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event) {
+		Block block = event.getBlock();
+		Player player = event.getPlayer();
+		ItemStack itemInHand = player.getInventory().getItemInMainHand();
+
+		if (block.getType() == Material.REINFORCED_DEEPSLATE && itemInHand.containsEnchantment(Enchantment.SILK_TOUCH))
+			block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.REINFORCED_DEEPSLATE));
+	}
+}
