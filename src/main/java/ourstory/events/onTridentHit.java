cbd0c02935cf9e45138068a0d99af43bda012c3c
Guilhem <aze.adrarit@gmail.com>
2024-11-19 21:58:50 +0100

    Refactored a bit trident thunder event

--- src/main/java/ourstory/events/onTridentHit.java
+++ src/main/java/ourstory/events/onTridentHit.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this
- * license Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package ourstory.events;
 
 import org.bukkit.Bukkit;
@@ -14,6 +9,7 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Trident;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,57 +18,72 @@ import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.persistence.PersistentDataType;
 
-
-/**
- *
- * @author aurel
+/*
+ * Credit to @aurel
  */
 public class onTridentHit implements Listener {
 	private final NamespacedKey wipeTrident = new NamespacedKey(Bukkit.getPluginManager().getPlugin("OurStory"), "wipeTrident");
 
 	@EventHandler
 	public void onLaunch(ProjectileLaunchEvent e) {
-		if (e.getEntity().getType() != EntityType.TRIDENT)
+		Projectile trident = e.getEntity();
+
+		if (trident.getType() != EntityType.TRIDENT)
 			return;
-		if (!(e.getEntity().getShooter() instanceof Player))
+
+		if (!(trident.getShooter() instanceof Player))
 			return;
-		Player p = (Player) e.getEntity().getShooter();
+
+		Player p = (Player) trident.getShooter();
+
 		if (!p.getInventory().getItemInMainHand().getItemMeta().hasEnchant(Enchantment.CHANNELING))
 			return;
-		e.getEntity().getPersistentDataContainer().set(wipeTrident, PersistentDataType.INTEGER, 1);
+		trident.getPersistentDataContainer().set(wipeTrident, PersistentDataType.INTEGER, 1);
 	}
 
-
-	// Détecter quand un trident touche un bloc
+	/*
+	 * Detects when a trident hits a bloc
+	 */
 	@EventHandler
 	public void onTridentHitBlock(ProjectileHitEvent event) {
-		if (event.getEntity() instanceof Trident) {
-			if (event.getEntity().getPersistentDataContainer().has(wipeTrident, PersistentDataType.INTEGER)) {
-				if (event.getHitBlock() != null) {
-					Block hitBlock = event.getHitBlock();
-					Player player = (Player) event.getEntity().getShooter();
-					if (hitBlock.getType() == Material.LIGHTNING_ROD) {
-						if (player.getWorld().hasStorm())
-							return;
-						player.getWorld().strikeLightningEffect(event.getEntity().getLocation());
-						player.getWorld().playSound(event.getEntity().getLocation(), Sound.ITEM_TRIDENT_THUNDER, 1000, 1);
-					}
-				}
-			}
+		Projectile trident = event.getEntity();
+
+		if (!(trident instanceof Trident))
+			return;
+
+		if (!trident.getPersistentDataContainer().has(wipeTrident, PersistentDataType.INTEGER))
+			return;
+
+		Block hitBlock = event.getHitBlock();
+
+		if (hitBlock == null)
+			return;
+
+		Player player = (Player) trident.getShooter();
+
+		if (hitBlock.getType() == Material.LIGHTNING_ROD) {
+			player.getWorld().strikeLightningEffect(event.getEntity().getLocation());
+			player.getWorld().playSound(event.getEntity().getLocation(), Sound.ITEM_TRIDENT_THUNDER, 1000, 1);
 		}
 	}
 
-	// Détecter quand un trident inflige des dégâts à une entité
+	/*
+	 * Triggers when a trident deals damage to an entity
+	 */
 	@EventHandler
 	public void onTridentHitEntity(EntityDamageByEntityEvent event) {
-		if (event.getDamager() instanceof Trident) {
-			if (event.getDamager().getPersistentDataContainer().has(wipeTrident, PersistentDataType.INTEGER)) {
-				Entity target = event.getEntity();
-				if (target.getWorld().hasStorm())
-					return;
-				target.getWorld().strikeLightningEffect(event.getEntity().getLocation());
-				target.getWorld().playSound(event.getEntity().getLocation(), Sound.ITEM_TRIDENT_THUNDER, 1000, 1);
-			}
-		}
+		Entity damager = event.getDamager();
+
+		if (!(damager instanceof Trident))
+			return;
+
+		if (!damager.getPersistentDataContainer().has(wipeTrident, PersistentDataType.INTEGER))
+			return;
+
+		Entity target = event.getEntity();
+
+		// Fire the lightning
+		target.getWorld().strikeLightningEffect(event.getEntity().getLocation());
+		target.getWorld().playSound(event.getEntity().getLocation(), Sound.ITEM_TRIDENT_THUNDER, 1000, 1);
 	}
 }
