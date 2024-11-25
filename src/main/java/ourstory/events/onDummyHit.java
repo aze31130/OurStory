cd327dbedd91c642fc29fd934b881b5f70d7beba
Draze999 <draze999@gmail.com>
2024-11-25 01:46:04 +0100

    Added Impaling, Smite and Bane Support for the Dummy

--- src/main/java/ourstory/events/onDummyHit.java
+++ src/main/java/ourstory/events/onDummyHit.java
@@ -1,13 +1,24 @@
 package ourstory.events;
 
+import java.util.Map;
+
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 
 public class onDummyHit implements Listener {
+
+	private final Map<String, Enchantment> EnchantmentsSensitivity = Map.of(
+			"sensitive_smite", Enchantment.SMITE,
+			"sensitive_boa", Enchantment.BANE_OF_ARTHROPODS,
+			"sensitive_impaling", Enchantment.IMPALING);
+
 	@EventHandler
 	public void dummyHit(EntityDamageByEntityEvent event) {
 		// Cancel if not player
@@ -15,12 +26,19 @@ public class onDummyHit implements Listener {
 			return;
 
 		// Cancel if not dummy
-		if (event.getEntity().getMetadata("isDummy").size() == 0)
+		if (event.getEntity().getMetadata("isDummy").isEmpty())
 			return;
 
 		event.setCancelled(true);
 
+		double base_damage = event.getDamage();
+		ItemStack itemInHand = ((Player) event.getDamager()).getInventory().getItemInMainHand();
+		for (String str : EnchantmentsSensitivity.keySet()) {
+			if (!event.getEntity().getMetadata(str).isEmpty())
+				base_damage += 2.5 * (itemInHand.containsEnchantment(EnchantmentsSensitivity.get(str)) ? itemInHand.getEnchantmentLevel(EnchantmentsSensitivity.get(str)) : 0);
+		}
+
 		Player p = (Player) event.getDamager();
-		p.sendMessage(Component.text("Dmg ").append(Component.text(String.format("%.2f", event.getDamage())).color(NamedTextColor.RED)));
+		p.sendMessage(Component.text(event.getEntity().getCustomName() + " : ").append(Component.text(String.format("%.2f", base_damage)).color(NamedTextColor.RED)));
 	}
 }
