6542c1bbb415c0b69285d44bb2e815b2d4f91acd
Guilhem <aze.adrarit@gmail.com>
2024-11-26 21:41:19 +0100

    Added arrow damage meter on dummy

--- src/main/java/ourstory/events/onDummyHit.java
+++ src/main/java/ourstory/events/onDummyHit.java
@@ -1,9 +1,9 @@
 package ourstory.events;
 
 import java.util.Map;
-
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,7 +22,37 @@ public class onDummyHit implements Listener {
 			"sensitive_impaling", Enchantment.IMPALING);
 
 	@EventHandler
-	public void dummyHit(EntityDamageByEntityEvent event) {
+	public void dummyHitArrow(EntityDamageByEntityEvent event) {
+		// Cancel if not arrow
+		if (!(event.getDamager() instanceof Arrow))
+			return;
+
+		// Cancel if not dummy
+		if (event.getEntity().getMetadata("isDummy").isEmpty())
+			return;
+
+		event.setCancelled(true);
+
+		Arrow arrow = (Arrow) event.getDamager();
+
+		// Cancel if shooter is not player
+		if (!(arrow.getShooter() instanceof Player))
+			return;
+
+		Player shooter = (Player) arrow.getShooter();
+		double baseDamage = event.getDamage();
+
+		// Apply "final_damage" enchantment
+		ItemStack bow = shooter.getInventory().getItemInMainHand();
+		baseDamage *= 1 + (0.05 * EnchantItem.getEnchantAmount(bow, "final_damage"));
+
+		// Send a message to the player with the calculated damage
+		shooter.sendMessage(Component.text(event.getEntity().getName() + " : ").append(Component.text(String.format("%.2f", baseDamage)).color(NamedTextColor.RED)));
+		arrow.remove();
+	}
+
+	@EventHandler
+	public void dummyHitPhysical(EntityDamageByEntityEvent event) {
 		// Cancel if not player
 		if (!(event.getDamager() instanceof Player))
 			return;
@@ -34,22 +64,21 @@ public class onDummyHit implements Listener {
 		event.setCancelled(true);
 
 		double base_damage = event.getDamage();
-		ItemStack itemInHand = ((Player) event.getDamager()).getInventory().getItemInMainHand();
+		Player p = (Player) event.getDamager();
+		ItemStack itemInHand = p.getInventory().getItemInMainHand();
 
 		// Ajout d'un moyen de suppression d'un dummy
 		if (itemInHand.getType().equals(Material.COD) && event.getDamager().isOp())
 			event.getEntity().remove();
 
 		// Ajout des dégâts pour chaque enchants présent sur le dummy et la meta data
-		for (String str : EnchantmentsSensitivity.keySet()) {
+		for (String str : EnchantmentsSensitivity.keySet())
 			if (!event.getEntity().getMetadata(str).isEmpty())
 				base_damage += 2.5 * (itemInHand.containsEnchantment(EnchantmentsSensitivity.get(str)) ? itemInHand.getEnchantmentLevel(EnchantmentsSensitivity.get(str)) : 0);
-		}
 
 		// Gestion de final_damage
 		base_damage *= 1 + (0.05 * EnchantItem.getEnchantAmount(itemInHand, "final_damage"));
 
-		Player p = (Player) event.getDamager();
 		p.sendMessage(Component.text(event.getEntity().getName() + " : ").append(Component.text(String.format("%.2f", base_damage)).color(NamedTextColor.RED)));
 	}
 }
