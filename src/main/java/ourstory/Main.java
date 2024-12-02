54edfd86096a5c3fb7fbf5e68ef85bbb10031b91
Draze999 <draze999@gmail.com>
2024-12-02 21:55:34 +0100

    Added Reach enchantment

--- src/main/java/ourstory/Main.java
+++ src/main/java/ourstory/Main.java
@@ -51,7 +51,7 @@ public class Main extends JavaPlugin {
 		 * Registers all events
 		 */
 		Listener[] eventsToRegister = {
-				new onArrowRain(), new onBossDeath(), new onBossHit(), new onDummyHit(), new onEntityDeath(),
+				new onReachEquip(), new onArrowRain(), new onBossDeath(), new onBossHit(), new onDummyHit(), new onEntityDeath(),
 				new onEntityHit(), new onFinalDamage(), new onHeadDrop(), new onItemConsume(),
 				new onMineAmethyst(), new onMineDeepslate(), new onPhoenixDeath(), new onPlayerDeath(),
 				new onPlayerInteract(), new onPlayerJoin(), new onPlayerPlace(), new onPlayerSit(),
