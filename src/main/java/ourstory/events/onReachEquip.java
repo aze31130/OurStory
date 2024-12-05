cbe376240e811ea9c68c0b05fcfbe553d6879581
Aurélien MASSET <71888778+Draze999@users.noreply.github.com>
2024-12-05 22:38:32 +0100

    Added Reach enchantment (#17)
    Co-authored-by: Draze999 <draze999@gmail.com>

---
+++ src/main/java/ourstory/events/onReachEquip.java
@@ -0,0 +1,44 @@
+package ourstory.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
+
+import ourstory.utils.EnchantItem;
+
+/**
+ *
+ * @author aurel
+ */
+public class onReachEquip implements Listener {
+
+	@EventHandler
+	public void onPlayerChangeArmor(PlayerArmorChangeEvent event) {
+		Player player = event.getPlayer();
+		int totalReachLevels = 0;
+		ItemStack[] armorContents = player.getInventory().getArmorContents();
+
+		for (ItemStack armor : armorContents)
+			totalReachLevels += EnchantItem.getEnchantAmount(armor, "reach");
+
+		if (totalReachLevels > 4)
+			totalReachLevels = 4;
+		applyReachModifier(player, totalReachLevels);
+	}
+
+	private void applyReachModifier(Player player, double ReachLevel) {
+		NamespacedKey ReachKey = new NamespacedKey(Bukkit.getPluginManager().getPlugin("OurStory"), "reach_bonus");
+		if (player.getAttribute(Attribute.PLAYER_BLOCK_INTERACTION_RANGE) != null)
+			player.getAttribute(Attribute.PLAYER_BLOCK_INTERACTION_RANGE).removeModifier(ReachKey);
+
+		AttributeModifier buff = new AttributeModifier(ReachKey, ReachLevel, AttributeModifier.Operation.ADD_NUMBER);
+		player.getAttribute(Attribute.PLAYER_BLOCK_INTERACTION_RANGE).addModifier(buff);
+	}
+}
