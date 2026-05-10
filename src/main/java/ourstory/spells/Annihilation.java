17756e1f9738f189e4cf8a53b128cecbc829428c
Elouan <elouan.hubert@epita.fr>
2026-05-10 18:17:52 +0100

    add shouldstop() in Spell + refacto of src/main/java/ourstory/spells/Annihilation.java (not tested)

--- src/main/java/ourstory/spells/Annihilation.java
+++ src/main/java/ourstory/spells/Annihilation.java
@@ -1,13 +1,30 @@
 package ourstory.spells;
 
 import java.util.List;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+
 
 public class Annihilation extends Spell {
 	/*
 	 * Special skill, last skill only used in chaos mode. Triggers at 20% HP remaining. The boss invokes
 	 * a flame circle arround him and deals a lot of damages.
 	 */
+	private Location loc;
+	private int pointCount;
+	private double radius;
+	private int totalSteps;
+	private double radiusIncrease;
+	private double startRadius;
+	private double endRadius;
+	private int t;
+
 	public Annihilation(Entity caster, List<Entity> targets, int level) {
 		super(caster, targets, level);
 	}
@@ -15,41 +32,71 @@ public class Annihilation extends Spell {
 	@Override
 	void setup() {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'setup'");
+		Location loc = caster.getLocation();
+		int pointCount = 6;
+		double radius = 20;
+		int totalSteps = 40;
+		double radiusIncrease = 0.5;
+		double startRadius = 0.5;
+		double endRadius = 20;
+		int t = 0;
+		// throw new UnsupportedOperationException("Unimplemented method 'setup'");
 	}
 
 	@Override
 	void tick() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'tick'");
+		for (int i = 0; i < pointCount; i++) {
+			double angle = 2 * Math.PI * i / pointCount;
+
+			int t = 0;
+
+			if (t > totalSteps) {
+				this.stop();
+				return;
+			}
+
+			double progress = (double) t / totalSteps;
+			double easedProgress = 1 - Math.pow(1 - progress, 2);
+
+			double currentRadius = radius * (1 - easedProgress);
+			double currentAngle = angle + Math.PI * easedProgress;
+
+			double x = Math.cos(currentAngle) * currentRadius;
+			double z = Math.sin(currentAngle) * currentRadius;
+			Location particleLoc = loc.clone().add(x, 0, z);
+
+			caster.getWorld().spawnParticle(Particle.LARGE_SMOKE, particleLoc, 0, 0, 0, 0, 0);
+			t++;
+		}
+
+		playCircleEffect(caster, loc, startRadius);
+		startRadius += radiusIncrease;
+		List<Entity> nearbyEntities = caster.getNearbyEntities(startRadius, 2, startRadius);
+		for (Entity entity : nearbyEntities) {
+			// Push the entity
+			Vector direction = entity.getLocation().toVector().subtract(loc.toVector()).normalize();
+			entity.setVelocity(direction.multiply(0.7));
+
+			// Damage if player
+			if (entity instanceof Player) {
+				Player player = (Player) entity;
+				player.damage(7.0);
+			}
+		}
+
+		// throw new UnsupportedOperationException("Unimplemented method 'tick'");
 	}
+
 
 	@Override
-	void stop() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'stop'");
+	boolean shouldStop() {
+		return (startRadius >= endRadius) || (t > totalSteps);
 	}
 
-	// @Override
-	// public void cast(Entity caster, List<Entity> targets, int level) {
-	// Location loc = caster.getLocation();
-
-	// int pointCount = 6;
-	// double radius = 20;
-	// int totalSteps = 40;
-
-	// // while true {
-	// // logque métier
-
-	// // break;
-	// // }
-
-	// // ===
 
 
-	// // while true {
-	// // logque métier
-	// // logque métier
+	@Override
+	void stop() {}
 
 	// // setup
 
@@ -57,77 +104,16 @@ public class Annihilation extends Spell {
 
 	// // stop
 
-	// // break;
-	// // }
 
-	// for (int i = 0; i < pointCount; i++) {
-	// double angle = 2 * Math.PI * i / pointCount;
 
-	// new BukkitRunnable() {
-	// int t = 0;
+	private static void playCircleEffect(Entity caster, Location loc, double radius) {
+		for (double angle = 0; angle < 2 * Math.PI; angle += Math.PI / 150) {
+			final double x = radius * Math.cos(angle);
+			final double z = radius * Math.sin(angle);
 
-	// @Override
-	// public void run() {
-	// if (t > totalSteps) {
-	// this.cancel();
-	// return;
-	// }
-
-	// double progress = (double) t / totalSteps;
-	// double easedProgress = 1 - Math.pow(1 - progress, 2);
-
-	// double currentRadius = radius * (1 - easedProgress);
-	// double currentAngle = angle + Math.PI * easedProgress;
-
-	// double x = Math.cos(currentAngle) * currentRadius;
-	// double z = Math.sin(currentAngle) * currentRadius;
-	// Location particleLoc = loc.clone().add(x, 0, z);
-
-	// caster.getWorld().spawnParticle(Particle.LARGE_SMOKE, particleLoc, 0, 0, 0, 0, 0);
-	// t++;
-	// }
-	// }.runTaskTimer(p, i * 2L, 2L);
-	// }
-
-	// new BukkitRunnable() {
-	// double radiusIncrease = 0.5;
-	// double startRadius = 0.5;
-	// double endRadius = 20;
-
-	// @Override
-	// public void run() {
-	// if (startRadius >= endRadius) {
-	// this.cancel();
-	// return;
-	// }
-
-	// playCircleEffect(caster, loc, startRadius);
-	// startRadius += radiusIncrease;
-
-	// List<Entity> nearbyEntities = caster.getNearbyEntities(startRadius, 2, startRadius);
-	// for (Entity entity : nearbyEntities) {
-	// // Push the entity
-	// Vector direction = entity.getLocation().toVector().subtract(loc.toVector()).normalize();
-	// entity.setVelocity(direction.multiply(0.7));
-
-	// // Damage if player
-	// if (entity instanceof Player) {
-	// Player player = (Player) entity;
-	// player.damage(7.0);
-	// }
-	// }
-	// }
-	// }.runTaskTimer(plugin, 100, 1);
-	// }
-
-	// private static void playCircleEffect(Entity caster, Location loc, double radius) {
-	// for (double angle = 0; angle < 2 * Math.PI; angle += Math.PI / 150) {
-	// final double x = radius * Math.cos(angle);
-	// final double z = radius * Math.sin(angle);
-
-	// loc.add(x, 0, z);
-	// caster.getWorld().spawnParticle(Particle.FLAME, loc, 1, 0, 0, 0, 0);
-	// loc.subtract(x, 0, z);
-	// }
-	// }
+			loc.add(x, 0, z);
+			caster.getWorld().spawnParticle(Particle.FLAME, loc, 1, 0, 0, 0, 0);
+			loc.subtract(x, 0, z);
+		}
+	}
 }
