0b8cb00750cc76208b9a942ba3710c310ee7a25b
Elouan <elouan.hubert@epita.fr>
2026-05-10 23:58:25 +0100

    refactor src/main/java/ourstory/spells/ArrowWall.java

--- src/main/java/ourstory/spells/ArrowWall.java
+++ src/main/java/ourstory/spells/ArrowWall.java
@@ -5,62 +5,59 @@ import java.util.Random;
 import org.bukkit.Location;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 public class ArrowWall extends Spell {
+	// TODO give arrow a random effect
+
+	private List<Entity> targets;
+	private Entity caster;
+	private int level, cpt, cptmax, speed;
+	private Location bossLocation;
+	private Entity target;
 
 	public ArrowWall(Entity caster, List<Entity> targets, int level) {
 		super(caster, targets, level);
+		this.targets = targets;
+		this.caster = caster;
+		this.level = level;
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void setup() {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'setup'");
+		this.cpt = 0;
+		this.cptmax = 80 * level;
+		this.speed = 3 * level;
 	}
 
 	@Override
 	public void tick() {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'tick'");
+
+		this.bossLocation = caster.getLocation().add(0, 4, 0);
+		this.target = targets.get(new Random().nextInt(targets.size()));
+		Vector direction = new Vector(
+				target.getX() - bossLocation.getX(),
+				target.getY() - bossLocation.getY(),
+				target.getZ() - bossLocation.getZ());
+
+		// Spawn the arrow at the boss's location with the calculated direction
+		Arrow arrow = caster.getWorld().spawnArrow(bossLocation, direction, 2, 6);
+		arrow.setLifetimeTicks(1100);
+		arrow.setCritical(true);
+		arrow.setPierceLevel(4);
+		arrow.setGravity(false);
+		cpt++;
 	}
 
 	@Override
-	public void stop() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'stop'");
-	}
+	public void stop() {}
 
 	@Override
 	public boolean shouldStop() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'shouldStop'");
+		return cpt > cptmax;
 	}
 
-	// @Override
-	// public void cast(Entity caster, List<Entity> targets, int level) {
-	// Location bossLocation = caster.getLocation().add(0, 4, 0);
-	// Entity target = targets.get(new Random().nextInt(targets.size()));
-
-	// for (int i = 0; i < 80; i++) {
-	// new BukkitRunnable() {
-	// @Override
-	// public void run() {
-	// Vector direction = new Vector(
-	// target.getX() - bossLocation.getX(),
-	// target.getY() - bossLocation.getY(),
-	// target.getZ() - bossLocation.getZ());
-
-	// // Spawn the arrow at the boss's location with the calculated direction
-	// Arrow arrow = caster.getWorld().spawnArrow(bossLocation, direction, 2, 6);
-	// arrow.setLifetimeTicks(1100);
-	// arrow.setCritical(true);
-	// arrow.setPierceLevel(4);
-	// arrow.setGravity(false);
-	// }
-	// }.runTaskLater(plugin, i / 20); // Slight delay for each arrow
-	// }
-	// }
 }
