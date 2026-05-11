b3eb08c2cd6cb2d9f8c024f992f761979611adfb
Elouan <elouan.hubert@epita.fr>
2026-05-11 12:32:49 +0100

    add random (bad) potions effect to arrow

--- src/main/java/ourstory/spells/ArrowWall.java
+++ src/main/java/ourstory/spells/ArrowWall.java
@@ -2,19 +2,30 @@ package ourstory.spells;
 
 import java.util.List;
 import java.util.Random;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
+import org.bukkit.potion.PotionType;
 import org.bukkit.util.Vector;
+import net.kyori.adventure.text.Component;
+
 
 public class ArrowWall extends Spell {
-	// TODO give arrow a random effect
+	// Spell test = new ArrowWall(player, player.getNearbyEntities(20, 5, 20), 3);
+	/*
+	 * if still called with getNearbyEntities, do not put a hugh y argument, it will only shoot on the
+	 * floor (entity in caves...)
+	 */
 
 	private List<Entity> targets;
 	private Entity caster;
-	private int level, cpt, cptmax, speed;
+	private int level, cpt, cptmax, speed, effectduration;
 	private Location bossLocation;
 	private Entity target;
+
+
+	private List<PotionType> arroweffect;
 
 	public ArrowWall(Entity caster, List<Entity> targets, int level) {
 		super(caster, targets, level);
@@ -27,9 +38,21 @@ public class ArrowWall extends Spell {
 	@Override
 	public void setup() {
 		// TODO Auto-generated method stub
+
+		Bukkit.getServer().broadcast(Component.text("number of targets =" + targets.size()));
 		this.cpt = 0;
 		this.cptmax = 80 * level;
 		this.speed = 3 * level;
+		this.arroweffect = List.of(
+				PotionType.LONG_SLOWNESS,
+				PotionType.LONG_WEAKNESS,
+				PotionType.HARMING,
+				PotionType.INFESTED,
+				PotionType.LONG_POISON,
+				PotionType.OOZING,
+				PotionType.AWKWARD);
+
+
 	}
 
 	@Override
@@ -42,13 +65,13 @@ public class ArrowWall extends Spell {
 				target.getX() - bossLocation.getX(),
 				target.getY() - bossLocation.getY(),
 				target.getZ() - bossLocation.getZ());
-
 		// Spawn the arrow at the boss's location with the calculated direction
-		Arrow arrow = caster.getWorld().spawnArrow(bossLocation, direction, 2, 6);
+		Arrow arrow = caster.getWorld().spawnArrow(bossLocation, direction, this.speed, 6);
 		arrow.setLifetimeTicks(1100);
 		arrow.setCritical(true);
 		arrow.setPierceLevel(4);
 		arrow.setGravity(false);
+		arrow.setBasePotionType(arroweffect.get(new Random().nextInt(arroweffect.size())));
 		cpt++;
 	}
 
