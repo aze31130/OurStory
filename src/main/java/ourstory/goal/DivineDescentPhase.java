1462b36f4476d1b35ddae3b7d7b03d0600199ca4
SushiCannibale <sushi.cannibale@gmail.com>
2026-05-11 15:28:14 +0200

    Adapted to new boss interface
    Signed-off-by: SushiCannibale <sushi.cannibale@gmail.com>

--- src/main/java/ourstory/goal/DivineDescentPhase.java
+++ src/main/java/ourstory/goal/DivineDescentPhase.java
@@ -1,13 +1,10 @@
 package ourstory.goal;
 
 import java.util.EnumSet;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.NamespacedKey;
-import org.bukkit.entity.Mob;
-import com.destroystokyo.paper.entity.ai.GoalKey;
+import org.bukkit.World;
+import org.bukkit.entity.BlockDisplay;
+import org.bukkit.util.Vector;
 import com.destroystokyo.paper.entity.ai.GoalType;
-import net.kyori.adventure.text.Component;
 import ourstory.bosses.HolyCow;
 
 /**
@@ -15,33 +12,65 @@ import ourstory.bosses.HolyCow;
  * sang froid par les joueurs.
  */
 public class DivineDescentPhase extends AbstractBossGoal<HolyCow> {
+	BlockDisplay glow1;
+	// BlockDisplay glow2;
+	// BlockDisplay glow3;
+	private float rot;
+
+	private static final float ANIM_STEP = 0.01f;
+
 	public DivineDescentPhase(final HolyCow boss) {
 		super(boss);
 	}
 
 	@Override
 	public boolean shouldActivate() {
+		System.out.println("State=" + boss.getState());
 		return boss.getState() == HolyCow.State.DESCENDING;
 	}
 
 	@Override
 	public boolean shouldStayActive() {
-		return boss.st
+		boolean stillInPhase = boss.getState() == HolyCow.State.DESCENDING;
+		boolean isOnGround = boss.entity.getLocation().toBlockLocation().isBlock();
+		System.out.println(String.format("StayActive=%b", stillInPhase && !isOnGround));
+		return (stillInPhase && !isOnGround);
+	}
+
+	void setupTransformations(BlockDisplay disp) {
+		var trans = disp.getTransformation();
+		trans.getTranslation().add(-0.5f, 0.0f, -0.5f);
+		trans.getLeftRotation().rotateAxis((float) Math.toRadians(45.0f), 0.0f, 1.0f, 0.0f);
+		// https://misode.github.io/transformation/
+	}
+
+	void tickRotation(BlockDisplay disp) {
+		var trans = disp.getTransformation();
+		trans.getLeftRotation().rotateAxis((float) Math.toRadians(rot), 0.0f, 1.0f, 0.0f);
+		rot = (rot + ANIM_STEP) % 360.0f;
 	}
 
 	@Override
 	public void start() {
 		boss.entity.teleport(boss.entity.getLocation().add(0, 100, 0));
+		boss.entity.setGravity(false);
+		boss.entity.setAI(false);
+		boss.entity.setVelocity(new Vector(0, -1, 0));
+		World world = boss.entity.getWorld();
+		this.glow1 = world.spawn(boss.entity.getLocation(), BlockDisplay.class);
+		this.rot = 0.0f;
+		setupTransformations(glow1);
 	}
 
 	@Override
 	public void tick() {
-		Bukkit.getServer().broadcast(Component.text("[SleepGoal] - zZz...zZz"));
+		tickRotation(glow1);
 	}
 
 	@Override
 	public void stop() {
-
+		boss.entity.setGravity(true);
+		boss.entity.setAI(true);
 	}
 
 	@Override
