94280c4f6e72ce46579827ca9e35d0c6453d217f
Elouan <elouan.hubert@epita.fr>
2026-05-28 10:47:33 +0100

    refactor src/main/java/ourstory/spells/WitherRage.java

--- src/main/java/ourstory/spells/WitherRage.java
+++ src/main/java/ourstory/spells/WitherRage.java
@@ -6,60 +6,55 @@ import org.bukkit.Particle;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.WitherSkull;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 public class WitherRage extends Spell {
 	public WitherRage(Entity caster, List<Entity> targets, int level) {
 		super(caster, targets, level);
-		// TODO Auto-generated constructor stub
 	}
+
+	private Entity target;
+	private int cpt, stop;
 
 	@Override
 	public void setup() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'setup'");
+		this.target = targets.get(0);
+		this.cpt = 0;
+		this.stop = cpt + 5 * level;
+		target.getWorld().spawnParticle(Particle.EXPLOSION_EMITTER, target.getLocation(), 5);
 	}
 
 	@Override
 	public void tick() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'tick'");
+		Location bossLocation = caster.getLocation();
+		Location playerLocation = target.getLocation();
+
+		Vector direction = playerLocation.toVector().subtract(bossLocation.toVector()).normalize();
+
+		// Spawn the WitherSkull at the boss's location
+		WitherSkull skull = (WitherSkull) caster.getWorld().spawnEntity(caster.getLocation().add(0, 1, 0), EntityType.WITHER_SKULL);
+
+		// Set the direction of the WitherSkull to point towards the player
+		skull.setDirection(direction);
+		skull.setAcceleration(direction.multiply(0.1));
+		cpt++;
+
 	}
 
 	@Override
-	public void stop() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'stop'");
-	}
+	public void stop() {}
 
 	@Override
 	public boolean shouldStop() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'shouldStop'");
+		return cpt >= stop;
 	}
 
 	// @Override
 	// public void cast(Entity caster, List<Entity> targets, int level) {
-	// Entity target = targets.get(0);
-	// target.getWorld().spawnParticle(Particle.EXPLOSION_EMITTER, target.getLocation(), 5);
-
 	// for (int i = 0; i < 15; i++) {
 	// new BukkitRunnable() {
 	// @Override
 	// public void run() {
-	// Location bossLocation = caster.getLocation();
-	// Location playerLocation = target.getLocation();
-
-	// Vector direction = playerLocation.toVector().subtract(bossLocation.toVector()).normalize();
-
-	// // Spawn the WitherSkull at the boss's location
-	// WitherSkull skull = (WitherSkull) caster.getWorld().spawnEntity(caster.getLocation().add(0, 1,
-	// 0), EntityType.WITHER_SKULL);
-
-	// // Set the direction of the WitherSkull to point towards the player
-	// skull.setDirection(direction);
-	// skull.setAcceleration(direction.multiply(0.1));
 	// }
 	// }.runTaskLater(plugin, 20 + i * 4);
 	// }
