e2e6d247e434a3c15b5b094ef40e3650ae95eed5
Guilhem <aze.adrarit@gmail.com>
2026-05-30 20:00:35 +0200

    Refactored talven phases

--- src/main/java/ourstory/Main.java
+++ src/main/java/ourstory/Main.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.json.JSONObject;
---
+++ src/main/java/ourstory/goal/TalvenPhase2.java
@@ -0,0 +1,65 @@
+package ourstory.goal;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
+import com.destroystokyo.paper.entity.ai.Goal;
+import com.destroystokyo.paper.entity.ai.GoalKey;
+import com.destroystokyo.paper.entity.ai.GoalType;
+import net.kyori.adventure.text.Component;
+import ourstory.bosses.Boss;
+import ourstory.spells.Spell;
+
+public final class TalvenPhase2 implements Goal<Mob> {
+	private final Boss boss;
+	private Player target;
+	private Location lastTargetLoc;
+
+	private final Set<Spell> spells;
+	/**
+	 * Last time (in server ticks) the behaviour has been triggered
+	 */
+	private Integer lastTickActivated;
+
+	public TalvenPhase2(final Boss boss, final Set<Spell> spells) {
+		this.boss = boss;
+		this.spells = spells;
+	}
+
+	@Override
+	public void start() {
+		Bukkit.getServer().broadcast(Component.text("[Phase2] - Setup "));
+	}
+
+	@Override
+	public void tick() {
+		Bukkit.getServer().broadcast(Component.text("[Phase2] - Running "));
+	}
+
+	@Override
+	public void stop() {
+		Bukkit.getServer().broadcast(Component.text("[Phase2] - Stop "));
+	}
+
+	@Override
+	public boolean shouldActivate() {
+		double currentHealth = (this.boss.entity.getHealth() / this.boss.entity.getAttribute(Attribute.MAX_HEALTH).getValue());
+		return currentHealth < 0.5 && currentHealth > 0.25;
+	}
+
+	@Override
+	public GoalKey<Mob> getKey() {
+		return GoalKey.of(Mob.class, new NamespacedKey("ourstory", "boss_phase2"));
+	}
+
+	@Override
+	public EnumSet<GoalType> getTypes() {
+		return EnumSet.of(GoalType.TARGET);
+	}
+}
