d9610677c6050807d63fb4c5e3291e0a66dfbfba
Guilhem <aze.adrarit@gmail.com>
2026-07-01 21:35:07 +0200

    Removed snowball damage

--- src/main/java/ourstory/events/onEntityHit.java
+++ src/main/java/ourstory/events/onEntityHit.java
@@ -1,7 +1,6 @@
 package ourstory.events;
 
 import org.bukkit.entity.Egg;
-import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -9,9 +8,6 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 public class onEntityHit implements Listener {
 	@EventHandler
 	public void entityHit(EntityDamageByEntityEvent entity) {
-		if ((entity.getDamager() instanceof Snowball))
-			entity.setDamage(1);
-
 		if ((entity.getDamager() instanceof Egg))
 			entity.setDamage(3);
 	}
